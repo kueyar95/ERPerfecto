9e71775e29f762b32cc1ed4bf12c65256d3e32a2
José Cuellar <67082016+kueyar95@users.noreply.github.com>
2022-05-26 09:18:45 -0400

    Delete JS directory

--- JS/CarritoCompras/js/app.js
+++
@@ -1,8 +0,0 @@
-//Variables
-const carrito = document.querySelector('#carrito'),
-      listaCurso = document.querySelector('#lista-curso tbody'),
-      contenedorCarrito = document.querySelector('#lista-carrito'),
-      vaciarCarrito = document.querySelector('#vaciar-carrito');
-
-
-function 
