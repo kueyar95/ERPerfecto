498ba4ca597682b1cae758033315d2fd5ed3f88e
José Cuellar <67082016+kueyar95@users.noreply.github.com>
2022-05-26 09:19:41 -0400

    Delete Node/uptask directory

--- Node/uptask/config/email.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    user: '392eb7aaaf1be9',
-    password: '0342a62ef76ba0',
-    host: 'smtp.mailtrap.io',
-    port: 2525
-}
--- Node/uptask/public/js/app.js
+++
@@ -1,7 +0,0 @@
-import proyectos from './modules/proyectos'
-import tareas from './modules/tareas'
-import {actualizarAvance} from './functions/avance'
-
-document.addEventListener('DOMContentLoaded', ()=>{
-    actualizarAvance();
-})
